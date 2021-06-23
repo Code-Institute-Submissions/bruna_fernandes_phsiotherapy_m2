8e64c8235a9125bae7f0444780067f4c61227e8c
Elippsis007 <michaeldrmm@gmail.com>
2021-06-23 19:09:52 +0000

    added styling to the datepicker

--- assets/javaScript/calendar.js
+++ assets/javaScript/calendar.js
@@ -12,7 +12,26 @@ const dates_element = document.querySelector('.date-picker .dates');
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 // Function Codes
-
+// toggleDatePicker is passed through (e) Event
 function toggleDatePicker (e) {
-    dates_element.classList.toggle('active');
+    if (!checkEventPathForClass(e.path, 'dates')){
+        dates_element.classList.toggle('active');
+    }
+    
+}
+
+// Helper Functions
+
+//This function will check the EVENT path so where I click, it will give me the path
+// of where I clicked. I if I clicked an element in the DatePicker it will tell me the whole
+// path of what the parents are of every single file from the element I clicked.
+function checkEventPathForClass (path, selector) {
+    for (let i=0; i < path.length; i++) {
+// Here I loop through every single item in the path and check that the class exist and then it checks the 
+// class path list which contains the selector which it will pass through!
+        if (path[i].classList && path[i].classList.contains(selector)) {
+            return true;
+        }
+    }
+    return false;
 }
