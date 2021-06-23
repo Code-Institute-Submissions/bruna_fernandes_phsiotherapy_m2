40181208d51738ffd69db61d56ab2b21273c2ac7
Elippsis007 <michaeldrmm@gmail.com>
2021-06-23 21:06:58 +0000

    created more JS to get months and current dates to display on calendar generated from JS code

--- assets/javaScript/calendar.js
+++ assets/javaScript/calendar.js
@@ -5,11 +5,37 @@ const selected_date_element = document.querySelector('date-picker .selected-date
 
 const dates_element = document.querySelector('.date-picker .dates');
 
+// To get more elements
+
+const mth_element = document.querySelector('.date-picker .dates .month .mth');
+const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
+const prev_element = document.querySelector('.date-picker .dates .month .prev-mth');
+const days_element = document.querySelector('.date-picker .dates .days');
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// To give us the current day
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+let selectedDate = date;
+let selectedDay = date;
+let selectedMonth = date;
+let selectedYear = date;
+
+// Here I'm getting the month and it will return which month it is which will be June and then add the Year to the string.
+mth_element.textContent = months[month] + ' ' + year;
+
 
 // Event Listener codes
 
 // Setting up a toggle by using Event Listeners upon click!
 date_picker_element.addEventListener('click', toggleDatePicker);
+// Now I will get the next month element
+next_mth_element.addEventListener('click', goToNextMonth)
+
+
 
 // Function Codes
 // toggleDatePicker is passed through (e) Event
