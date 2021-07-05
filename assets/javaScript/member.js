6da1d8de812a933ca6a9287800b01c1cdf7c13dd
Elippsis007 <michaeldrmm@gmail.com>
2021-07-05 17:10:05 +0000

    added popup overlay on click with JavaScript

--- assets/javaScript/member.js
+++ assets/javaScript/member.js
@@ -1,14 +1,17 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "inline") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "inline";
-    }
-  });
-}
+//Use the onload event so that we can make sure the DOM is at 
+//least mostly loaded before trying to get elements
+window.onload = function() {
+  //Get the DOM element that represents the <button> element.
+  //And set the onclick event
+  document.getElementById("LearnMoreBtn").onclick = function(){
+     //Set a variable to contain the DOM element of the overly
+     var overlay = document.getElementById("overlay");
+     //Set a variable to contain the DOM element of the popup
+     var popup = document.getElementById("popup");
+     
+     //Changing the display css style from none to block will make it visible
+     overlay.style.display = "block";
+     //Same goes for the popup
+     popup.style.display = "block";
+  };
+};
