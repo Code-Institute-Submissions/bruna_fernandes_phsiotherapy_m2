481304241c4c70cefe4dadfcbe2de189ce078a85
Elippsis007 <michaeldrmm@gmail.com>
2021-07-07 23:47:09 +0000

    finished the js code for all 12 images on memebers page, now I am adding price for members.

--- assets/javaScript/member.js
+++ assets/javaScript/member.js
@@ -1,15 +1,17 @@
+
+/*---------------------------CUPPING-----------------------------*/
 $(function() {
     //This function will execute as soon as page loads
         // It will grab the HTML element with the ID tag of "visible" and when
         // it is clicked it will execute another function and inside that function
         // it will grab the HTML element with ID tag of "invisible"
-	$("#visible").click(function() {
+	$("#visible1").click(function() {
          //This will be our toggle a class on and off which will be "show"
-	    $('#invisible').toggleClass("show");
+	    $('#invisible1').toggleClass("show");
 	  });  
 	});
 
-
+/*---------------------------DRY NEEDLING-----------------------------*/
     $(function() {
         //This function will execute as soon as page loads
             // It will grab the HTML element with the ID tag of "visible" and when
@@ -20,6 +22,78 @@ $(function() {
             $('#invisible2').toggleClass("show");
           });  
         });
+
+/*---------------------------ACCUPUNCTURE-----------------------------*/        
+        $(function() {
+          $("#visible3").click(function() {
+               $('#invisible3').toggleClass("show");
+            });  
+          });
+
+
+/*---------------------------DEEP TISSUE-----------------------------*/        
+$(function() {
+  $("#visible4").click(function() {
+       $('#invisible4').toggleClass("show");
+    });  
+  });
+
+/*---------------------------MASSAGE THERAPY-----------------------------*/        
+$(function() {
+  $("#visible5").click(function() {
+       $('#invisible5').toggleClass("show");
+    });  
+  });
+
+/*---------------------------PHYSIOTHERAPY-----------------------------*/        
+$(function() {
+  $("#visible6").click(function() {
+       $('#invisible6').toggleClass("show");
+    });  
+  });
+
+/*---------------------------ORTHOPAEDIC THERAPY-----------------------------*/        
+$(function() {
+  $("#visible7").click(function() {
+       $('#invisible7').toggleClass("show");
+    });  
+  });
+
+/*--------------------------BACK PAIN-----------------------------*/        
+$(function() {
+  $("#visible8").click(function() {
+       $('#invisible8').toggleClass("show");
+    });  
+  });
+
+/*---------------------------FEMALE HEALTH-----------------------------*/        
+$(function() {
+  $("#visible9").click(function() {
+       $('#invisible9').toggleClass("show");
+    });  
+  });
+
+/*---------------------------CHILD PHYSIOTHERAPY-----------------------------*/        
+$(function() {
+  $("#visible10").click(function() {
+       $('#invisible10').toggleClass("show");
+    });  
+  });
+
+/*---------------------------SPORT PHYSIOTHERAPY-----------------------------*/        
+$(function() {
+  $("#visible11").click(function() {
+       $('#invisible11').toggleClass("show");
+    });  
+  });
+
+/*---------------------------HOME VISIT PHYSIOTHERAPY-----------------------------*/        
+$(function() {
+  $("#visible12").click(function() {
+       $('#invisible12').toggleClass("show");
+    });  
+  });
+
 
         
 
